0db18ab44b8602dc49bc1d848d60fb770085235e
kamalika-boddu <kamalikaboddu@gmail.com>
2026-07-29 10:43:29 +0530

    Time: 52 ms (5.91%), Space: 46.9 MB (74.16%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Scanner sc = new Scanner(System.in);
+        
+        for (int i = 0;i<nums.length;i++)
+        {
+            for (int j = i+1;j<nums.length ; j++)
+            {
+                if(nums[j]+nums[i]== target){
+                return new int[] {i,j};
+                }
+
+            }
+        }
+        return new int[] {};
+    }
+}
