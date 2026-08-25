b2690b51b2011e1f41264a7445fcc0ecd9c8aefc
kamalika-boddu <kamalikaboddu@gmail.com>
2026-08-25 10:50:21 +0530

    Time: 0 ms (100%), Space: 44.7 MB (67.06%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int n= nums.length;
+        int low=0;
+        int high=n-1;
+        while(low<=high)
+        {int mid=low+(high-low)/2;
+            if(target==nums[mid])
+            {
+                return mid;
+            }
+            if(target<nums[mid])
+            {
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+            
+        }
+        return low;
+    }
+}
